a96ab2caacb531e808e3098356b1a9cd3d08cca2
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-06 21:20:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function writeCards(nameArray, celebration) {
-  for(name = 0, name < nameArray.length, name++) {
+  for(let name = 0, name < nameArray.length, name++) {
     console.log(`Thank you, ${nameArray[name]}, for the wonderful ${celebration} gift!`);
   }
 }
